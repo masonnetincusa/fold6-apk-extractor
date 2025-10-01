689f90bd5637ab10ab46b15e0011189aeacddcc1
MASONNET <masonmurphyslim@gmail.com>
2025-10-01 10:37:17 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 set -e
 
-echo "[+] Installing tools..."
+# Install required tools
 sudo apt update
 sudo apt install -y lz4 simg2img e2fsprogs unzip tar megatools
 
-echo "[+] Downloading fold6ap.zip from MEGA..."
+# Download from MEGA
 megadl "https://mega.nz/file/j5ACRbQL#1v6gDbftqpVlsXi6dLo_4DoxFSmiEH28mYDhOPBD-5Y"
 
-echo "[+] Extracting AP tar.md5..."
+# Extract AP tar.md5
 unzip fold6ap.zip "AP_F956B*.tar.md5"
 tar -xvf AP_F956B*.tar.md5 system.img.lz4
 
-echo "[+] Decompressing system.img.lz4..."
+# Decompress and convert
 lz4 -d system.img.lz4 system.img
 simg2img system.img system.raw.img
 
-echo "[+] Extracting APKs..."
+# Extract APKs
 debugfs -w system.raw.img <<EOF
 cd /system/priv-app/SmartSuggestions
 dump SmartSuggestions.apk SmartSuggestions.apk
@@ -24,5 +24,3 @@ cd /system/priv-app/AODService
 dump AODService.apk AODService.apk
 quit
 EOF
-
-echo "[✓] APKs extracted: SmartSuggestions.apk, AODService.apk"
